d2899b9c2a2bf6ba821b9d74e4efc5e9043903ca
ab-navarra <ab@navarra-lab.com>
2022-10-26 11:57:43 +0200

    Calculating confidence and refactoring

--- custom_adapter.py
+++ custom_adapter.py
@@ -1,55 +1,91 @@
 import re
 import struct
 
+from abc import ABCMeta, abstractmethod
+from typing import List
+
 from chatterbot.conversation import Statement
 from chatterbot.logic import LogicAdapter
 
 
-class HexColorLogicAdapter(LogicAdapter):
+class MyAdapter(LogicAdapter, metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def pattern(self) -> re.Pattern:
+        pass
+
+    @property
+    @abstractmethod
+    def keywords(self) -> List[str]:
+        pass
+
+    def can_process(self, statement: Statement):
+        return self.pattern.findall(statement.text)
+
+    @abstractmethod
+    def process(self, input_statement: Statement, additional_response_selection_parameters=None) -> Statement:
+        pass
+
+    def calculate_confidence(self, match: str, input_statement: Statement) -> float:
+        match_index = len(match) / len(input_statement.text)
+        keyword_index = self._contains_keyword(input_statement)
+        return match_index * 0.4 + keyword_index * 0.6
+
+    def _contains_keyword(self, input_statement: Statement):
+        return any(
+            keyword in [
+                word.lower()
+                for word in
+                input_statement.text.split()
+            ]
+            for keyword
+            in self.keywords
+        )
+
+
+class HexColorLogicAdapter(MyAdapter):
     pattern = re.compile(r'#([a-f\d]{6})', flags=re.IGNORECASE)
+    keywords = ['hex', 'hexadecimal', 'color', 'colour', 'rgb']
 
     def __init__(self, chatbot, **kwargs):
         super().__init__(chatbot, **kwargs)
-
-    def can_process(self, statement: Statement):
-        return self.pattern.match(statement.text)
 
     def process(self, input_statement: Statement, additional_response_selection_parameters=None):
         hex_number = self.pattern.findall(input_statement.text)[0]
         r, g, b = struct.unpack('BBB', bytes.fromhex(hex_number))
-        confidence = 0.0
 
-        text = 'I see, you mention color in HEX! Red: {}, Green: {}, Blue: {}'.format(r, g, b)
-        response_statement = Statement(text=text, confidence=confidence)
+        text = 'RGB of your color is: Red: {}, Green: {}, Blue: {}'.format(r, g, b)
+        response_statement = Statement(text=text)
+        response_statement.confidence = self.calculate_confidence(hex_number, input_statement)
 
         return response_statement
 
 
-class BinaryLogicAdapter(LogicAdapter):
+class BinaryLogicAdapter(MyAdapter):
     pattern = re.compile(r'([01]{2,})', flags=re.IGNORECASE)
+    keywords = ['binary', 'bin']
 
     def __init__(self, chatbot, **kwargs):
         super().__init__(chatbot, **kwargs)
 
-    def can_process(self, statement: Statement):
-        return self.pattern.match(statement.text)
-
     def process(self, input_statement: Statement, additional_response_selection_parameters=None):
         binary = self.pattern.findall(input_statement.text)[0]
-        confidence = 0.0
-        text = 'Gotcha! {}'.format(int(binary, 2))
-        response_statement = Statement(text=text, confidence=confidence)
+        text = f'Decimal value of {binary} is {int(binary, 2)}'
+
+        response_statement = Statement(text=text)
+        response_statement.confidence = self.calculate_confidence(binary, input_statement)
 
         return response_statement
 
 
-class BiologyComplementaryLogicAdapter(LogicAdapter):
+class BiologyComplementaryLogicAdapter(MyAdapter):
     pattern = re.compile(r'([actg]{3,})', flags=re.IGNORECASE)
+    keywords = ['gene', 'genes', 'genetic', 'genetics', 'code', 'sequence', 'complement', 'complementary']
     mapper = {
-        'A': 'U',
-        'C': 'G',
-        'T': 'A',
-        'G': 'C'
+        ord('A'): 'U',
+        ord('C'): 'G',
+        ord('T'): 'A',
+        ord('G'): 'C'
     }
 
     def replace(self, g):
@@ -58,12 +94,14 @@ class BiologyComplementaryLogicAdapter(LogicAdapter):
     def __init__(self, chatbot, **kwargs):
         super().__init__(chatbot, **kwargs)
 
-    def can_process(self, statement):
-        return self.pattern.match(statement.text)
-
     def process(self, input_statement, additional_response_selection_parameters=None):
-        text = self.pattern.sub(self.replace, input_statement.text)
-        confidence = 0.0
-        response_statement = Statement(text=text, confidence=confidence)
+        codes: List[str] = self.pattern.findall(input_statement.text)
+        text = ' '.join(
+            code.translate(self.mapper)
+            for code
+            in codes
+        )
+        response_statement = Statement(text=text)
+        response_statement.confidence = self.calculate_confidence(text, input_statement)
 
         return response_statement
--- main.py
+++ main.py
@@ -3,27 +3,27 @@ import time
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-
 time.clock = time.time
 
 # Create a new chatbot named Charlie
-chatbot = ChatBot('Neo',
-                  logic_adapters=[
-                      {
-                          'import_path': 'custom_adapter.BinaryLogicAdapter'
-                      },
-                      {
-                          'import_path': 'custom_adapter.HexColorLogicAdapter'
-                      },
-                      {
-                          'import_path': 'custom_adapter.BiologyComplementaryLogicAdapter'
-                      },
-                      {
-                          'import_path': 'chatterbot.logic.BestMatch',
-                          'default_response': 'Could you repeat please?',
-                          'maximum_similarity_threshold': 0.9
-                      }
-                  ])
+chatbot = ChatBot(
+    'Neo',
+    logic_adapters=[
+        {
+            'import_path': 'custom_adapter.BinaryLogicAdapter'
+        },
+        {
+            'import_path': 'custom_adapter.HexColorLogicAdapter'
+        },
+        {
+            'import_path': 'custom_adapter.BiologyComplementaryLogicAdapter'
+        },
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'Could you repeat please?',
+            'maximum_similarity_threshold': 0.9
+        }
+    ])
 
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot_corpus/train_basic.yml")
@@ -31,9 +31,9 @@ trainer.train("chatterbot_corpus/train_basic.yml")
 
 def ask(question):
     response = chatbot.get_response(question)
-    print(response)
+    print(f'> {question}\n{response}')
 
 
-ask('#FF00FF')
-ask('110011010')
-ask('ATG CTA AGC ATA')
+ask('What is binary value for #010011?')
+ask('What is RGB value for #010011?')
+ask('Could you show complementary sequence for "ATG CTA AGC ATA"?')
