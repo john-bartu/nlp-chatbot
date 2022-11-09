41f67a14e21b289c7ec9aea6172f26966037e9ee
jamabaiz <jan@bartulacode.pl>
2022-11-09 14:13:52 +0100

    Route to PK

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -104,18 +104,59 @@ class ActionScheduleForDay(Action):
         else:
             dispatcher.utter_message(template='utter_schedule_unknown_day_of_week')
         return []
-#
-# def get_route2(loc):
-#     source_loc = (50.0576542, 19.9454402)
-#     dest_loc = (50.0712524, 19.9386159)
-#     route, route_length, route_time = get_route(source_loc, dest_loc)
-#     print(f'{route = }')
-#     print(f'{route_length = }')
-#     print(f'{route_time = }')
-#
-#
-# if __name__ == '__main__':
-#     location2 = 'kek'
-#     print(location2)
-#     get_route2(location2)
-#     print('finished')
+
+
+class ActionRouteForDay(Action):
+
+    def name(self) -> Text:
+        return "action_get_route_for_day"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        day_of_week = str(tracker.get_slot('day_of_week')).lower()
+
+        mapper = {
+            'monday': 'Only remote lectures today',
+            'tuesday': 'Leave [ 16:35 ] -> Bus 703 [ 16:45 - 16:51 ] -> PK [ 16:55 ]\n'
+                       'or Leave [ 16:39 ] -> Bus 703 [ 16:48 - 16:54 ] -> PK [ 16:58 ]\n'
+                       'or Leave [ 16:42 ] -> Bus 703 [ 16:51 - 16:57 ] -> PK [ 17:01 ]',
+            'wednesday': 'Leave [ 14:35 ] -> Bus 703 [14:45 - 14:51] -> PK [14:55]\n'
+                         'or Leave [ 14:39 ] -> Bus 703 [ 14:48 - 14:54 ] -> PK [ 14:58 ]\n'
+                         'or Leave [ 14:42 ] -> Bus 703 [ 14:51 - 14:57 ] -> PK [ 14:01 ]',
+            'thursday': 'No lectures yay!',
+            'friday': 'Ouh, yeah. Only remote lectures today!',
+            'saturday': 'No lectures yay!',
+            'sunday': 'No lectures yay!',
+        }
+
+        if day_of_week in mapper:
+            dispatcher.utter_message(text=mapper[day_of_week])
+        else:
+            dispatcher.utter_message(template='utter_schedule_unknown_day_of_week')
+        return []
+
+
+class ActionGetSubjectInfo(Action):
+
+    def name(self) -> Text:
+        return "action_get_subject_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        subject = str(tracker.get_slot('subject')).lower()
+
+        mapper = {
+            'pjn': 'Przetwarzanie języka naturalnego\n\tdr Radosław Kycia',
+            'pm': 'Programowanie mobilne\n\tLecture: dr hab. Joanna Kołodziej, '
+                  'prof. PK\n\tLab: mgr inż. Michał Niedzwiecki',
+            'ajio': 'Automaty, języki i obliczenia\n\tdr inż. Jerzy Zaczek',
+            'sm': 'Seminarium dyplomowe\n\tprof. zw. dr hab. inż. Tadeusz Burczyński',
+        }
+
+        if subject in mapper:
+            dispatcher.utter_message(text=mapper[subject])
+        else:
+            dispatcher.utter_message(template='utter_subject_unknown')
+        return []
